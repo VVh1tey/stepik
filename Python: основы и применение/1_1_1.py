887be6b853b6295418119c6d9843bea520a93839
Vasiliy <65813774+VVh1tey@users.noreply.github.com>
2023-06-14 10:53:48 +0300

    Update 1_1_1.py

--- Python: основы и применение/1_1_1.py
+++ Python: основы и применение/1_1_1.py
@@ -1,1 +1,10 @@
-
+def sum_v1() -> int:
+    n,res = int(input()), 0
+    while(n):
+        res += int(input())
+        n-=1
+    print(res)
+def sum_v2() -> int:
+    print(sum([int(input()) for i in range(int(input()))]))
+sum_v1()
+#sum_v2()
